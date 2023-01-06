a344ac4a6e3a6b3bb23ef3d201332fe7e47dd5cc
Julia Morka <juliamorka@student.agh.edu.pl>
2023-01-06 20:05:20 +0100

    Fixed PHP stan issues.

--- project/app/Models/User.php
+++ project/app/Models/User.php
@@ -48,7 +48,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function setPasswordAttribute($value)
+    public function setPasswordAttribute(string $value): void
     {
         $this->attributes['password'] = Hash::make($value);
     }
